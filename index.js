20990733c2e20d555820f140f23352cc391536f1
Shanto Islam <rajs45693@gmail.com>
2023-03-15 22:16:53 +0600

    timer is ok

--- index.js
+++ index.js
@@ -23,8 +23,8 @@ startQuiz.addEventListener("click", () => {
 
   let x = setInterval(() => {
     if (counterNum < 0) {
-      coutDown.classList.remove("flex");
-      coutDown.classList.add("hidden");
+      countDown.classList.remove("flex");
+      countDown.classList.add("hidden");
       counterNum = 3;
       count = 0;
       timer = null;
@@ -46,25 +46,29 @@ startQuiz.addEventListener("click", () => {
 // All quiz data fetched from json
 const loadQuiz = async () => {
   const res = await fetch("./data/quiz.json");
-  const data = await res.json;
+  const data = await res.json();
+  
   quizData = data;
-  displayQuiz(data);
+  
+  displayQuiz(quizData);
 };
 
 // Displaying quiz on quiz page
 const displayQuiz = (data) => {
+  // console.log(data);
   if (!data) {
     quizContainer.innerHTML = "";
     return;
   }
 
   data.forEach((quiz, i) => {
+    
     quizContainer.innerHTML += `<div class="m-3 py-3 px-4 shadow-sm rounded">
   <div class="flex items-center">
     <div class="h-8 w-8 bg-green-300 rounded-full flex justify-center items-center text-green-800 mr-3">
       ${i + 1}
     </div>
-    <p class="text-gray-800 text-sm">${quiz.quetion}</p>
+    <p class="text-gray-800 text-sm">${quiz.question}</p>
   </div>
   <div class="grid grid-cols-2 gap-4 mt-5">
     ${displayQuizOptions(quiz.options, i)}
@@ -74,8 +78,8 @@ const displayQuiz = (data) => {
 };
 
 // EventListener for quiz submit button
-document.querySelector("#submit").addEventlistener("click", () => {
-  if (answers.length < 6) {
+document.querySelector("#submit").addEventListener("click", () => {
+  if (answers.length > 6) {
     return;
   }
   quizTimer(true);
